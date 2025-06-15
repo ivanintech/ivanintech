7edf80e62a2d5b0664b3e1f71c1d8cfcb311520b
Iván Castro Martínez <ivancastro.info@gmail.com>
2025-06-15 16:48:37 +0200

    fix migrations one to one

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -76,11 +76,11 @@ def run_migrations_online() -> None:
 
     with connectable.connect() as connection:
         context.configure(
-            connection=connection, target_metadata=target_metadata
+            connection=connection, target_metadata=target_metadata,
+            transaction_per_migration=True
         )
 
-        with context.begin_transaction():
-            context.run_migrations()
+        context.run_migrations()
 
 
 if context.is_offline_mode():
