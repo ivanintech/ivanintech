9479a2ed3ac4489ccfef9adaaf39c52bdbfe458f
Iván Castro Martínez <ivancastro.info@gmail.com>
2025-06-17 08:47:53 +0200

    fix alembic merge

---
+++ backend/alembic/versions/90eb69a0f500_merge_multiple_heads.py
@@ -0,0 +1,28 @@
+"""Merge multiple heads
+
+Revision ID: 90eb69a0f500
+Revises: 4e0475a8c061, d0ef336db381
+Create Date: 2025-06-17 08:46:35.152414
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '90eb69a0f500'
+down_revision: Union[str, None] = ('4e0475a8c061', 'd0ef336db381')
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
