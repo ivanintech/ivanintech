0d7091a60b29ecb7ef5accd0fa85a267b092abdb
Iván Castro Martínez <ivancastro.info@gmail.com>
2025-06-17 08:44:11 +0200

    fixing migrationssssss

--- backend/alembic/versions/d0ef336db381_add_likes_and_dislikes_to_resource_links.py
+++ backend/alembic/versions/d0ef336db381_add_likes_and_dislikes_to_resource_links.py
@@ -1,3 +1,13 @@
+"""Add likes and dislikes to resource_links
+
+Revision ID: d0ef336db381
+Revises: 9f0346115888
+Create Date: 2025-06-17 08:31:36.157035
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
 # ... existing code ...
 # revision identifiers, used by Alembic.
 revision: str = 'd0ef336db381'
