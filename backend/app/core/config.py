b0c305aef935e6b918530cf39f6c76cc096ca2a7
Iván Castro Martínez <ivancastro.info@gmail.com>
2025-06-23 06:08:58 +0200

    fix(config): Set default FRONTEND_HOST to localhost for local development

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -40,7 +40,7 @@ class Settings(BaseSettings):
     # 60 minutes * 24 hours * 8 days = 8 days
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
     SERVER_HOST: str = "http://localhost:8000"
-    FRONTEND_HOST: str
+    FRONTEND_HOST: str = "http://localhost:3000"
     ENVIRONMENT: Literal["local", "staging", "production"] = "local"
 
     BACKEND_CORS_ORIGINS: Annotated[
